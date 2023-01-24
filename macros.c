605f43d2f03223cd1254e291e9abb7da4964da26
ShFishman <123412193+ShFishman@users.noreply.github.com>
2023-01-24 09:26:53 -0500

    Add files via upload

---
+++ macros.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+struct Macro{
+
+}
+
+// A linked list node
+struct McrList {
+	int data;
+	struct Mcr_list* next;
+};
+struct McrList m1;
+m1.data = 1;
+m1.next = struct McrList m2;
+
+static bool search_mcr
+
+/*
+mcr m1 inc r2
+ mov A,r1
+endmcr 
+*/
